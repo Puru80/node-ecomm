fc7a57b275482a571b7b8a25b2d246dfead9c679
Puru80 <puru.agar99@gmail.com>
2025-02-27 20:50:28 +0530

    Category entity

---
+++ category/Category.js
@@ -0,0 +1,40 @@
+const EntitySchema = require("typeorm").EntitySchema;
+
+class Category {
+    constructor(name, description, created_at, updated_at) {
+        this.name = name;
+        this.description = description;
+        this.created_at = created_at;
+        this.updated_at = updated_at;
+    }
+}
+
+CategoryEntity = new EntitySchema({
+    name: "Category",
+    tableName: "categories",
+    columns: {
+        id: {
+            primary: true,
+            type: "int",
+            generated: true
+        },
+        name: {
+            type: "varchar",
+            unique: true
+        },
+        description: {
+            type: "text",
+            nullable: true
+        },
+        created_at: {
+            type: "timestamp",
+            createDate: true
+        },
+        updated_at: {
+            type: "timestamp",
+            updateDate: true
+        }
+    }
+})
+
+module.exports = {Category, CategoryEntity};
